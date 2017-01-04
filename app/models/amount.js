e65928c40964c88be34d21ea822e160b75b8ffe9
nlsltz <info@nilslutz.de>
2017-01-04 15:52:10 +0100

    Fixed typo

--- app/models/amount.js
+++ app/models/amount.js
@@ -3,13 +3,13 @@ var Schema = mongoose.Schema;
 var User = require('./user');
 
 var amountSchema = new Schema({
-	user: { type: Schema.ObjectId, required: true },
+	userId: { type: Schema.ObjectId, required: true },
 	type: { type: String },
-	amount: { type: Number }
+	amount: { type: Number, required: true }
 });
 
 amountSchema.pre('save', function(next) {
-	var tmpObjectId = this.ObjectId;
+	var tmpObjectId = this.user;
 
 	User.find({ _id: tmpObjectId }, function(err, user) {
 		if (err) throw err;
